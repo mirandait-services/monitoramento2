eecd4a9fdad9badd97841b84b48aa6d7eb0d357c
mirandait-services <78923269+mirandait-services@users.noreply.github.com>
2021-02-21 15:51:59 -0300

    v4

--- setup.sh
+++ setup.sh
@@ -14,5 +14,15 @@ git clone https://github.com/mirandait-services/monitoramento
 #echo IP_SERVER=http://$IP_SERVER:9000/ > monitoramento/ambiente/var.env
 IP_SERVER=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f7)
 echo IP_SERVER=http://$IP_SERVER:9000/ > monitoramento/ambiente/var.env
-
 docker-compose --env-file monitoramento/ambiente/var.env -f monitoramento/ambiente/docker-compose.yml up -d
+IP_SERVER=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f7)
+echo
+echo
+echo PARA ACESSAR O ZABBIX INSIRA O SEGUINTE ENDEREÇO EM SEU NAVEGADOR WEB: http://$IP_SERVER/
+IP_SERVER=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f7)
+echo
+echo
+echo PARA ACESSAR O GRAFANA INSIRA O SEGUINTE ENDEREÇO EM SEU NAVEGADOR WEB: http://$IP_SERVER:3000/
+echo
+echo
+echo PARA ACESSAR O GRAFANA INSIRA O SEGUINTE ENDEREÇO EM SEU NAVEGADOR WEB: http://$IP_SERVER:9000/
