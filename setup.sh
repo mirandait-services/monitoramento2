8f1ec2d7c6d6a038fad5e5b601499b16c07145fe
mirandait-services <78923269+mirandait-services@users.noreply.github.com>
2021-02-21 17:47:00 -0300

    fdsfd

--- setup.sh
+++ setup.sh
@@ -23,6 +23,7 @@ echo GRAYLOG_PASSWORD=$GRAYLOG_PASSWORD >> monitoramento2/var.env
 docker-compose --env-file monitoramento2/var.env -f monitoramento2/docker-compose.yml up -d
 IP_SERVER=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f7)
 echo > monitoramento2/var.env
+sleep 2m
 clear
 echo
 echo
