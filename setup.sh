ef414f38c1a33334ef9370dc6d8426d7fc090c76
edutmiranda <79800172+edutmiranda@users.noreply.github.com>
2021-02-28 14:54:06 -0300

    sdjfhnjd

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#Instalação dos pacotes necessários
 apt update
 apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -6,17 +7,19 @@ apt update
 apt install docker-ce -y
 curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-rm -r monitoramento2/
-docker rm -f $(docker ps -a -q)
-docker volume prune --force
-git clone https://github.com/mirandait-services/monitoramento2
-#read -p 'Por gentileza insira o endereço IP ou URL que sera utilizada para acesso ao Graylog: ' IP_SERVER
-#echo IP_SERVER=http://$IP_SERVER:9000/ > monitoramento2/var.env
+#Reset do ambiente
+#rm -r monitoramento2/
+#docker rm -f $(docker ps -a -q)
+#docker volume prune --force
+#git clone https://github.com/mirandait-services/monitoramento2
+#Identificação do IP principal do servidor de monitoramento
 IP_SERVER=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f7)
 echo IP_SERVER=http://$IP_SERVER:9000/ > monitoramento2/var.env
+#Password pepper aleatório para o Graylog
 RANDOMPEPPER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 echo RANDOMPEPPER=$RANDOMPEPPER >> monitoramento2/var.env
 clear
+#Configuração se senha de acesso ao Graylog
 echo Por gentileza insira a senha de acesso ao Graylog Server:
 GRAYLOG_PASSWORD=$(stty -echo && head -1 </dev/stdin | tr -d '\n' | sha256sum | cut -d" " -f1)
 echo GRAYLOG_PASSWORD=$GRAYLOG_PASSWORD >> monitoramento2/var.env
@@ -24,11 +27,13 @@ docker-compose --env-file monitoramento2/var.env -f monitoramento2/docker-compos
 IP_SERVER=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f7)
 echo > monitoramento2/var.env
 echo
+#Delay para término do setup
 echo "Aguarde enquanto as configurações personalizadas do ambiente são aplicadas"
-sleep 1m
+sleep 30s
 clear
 echo
 echo
+#Fim da instalação e configuração
 echo 'INSTALAÇÃO CONCLUÍDA, PARA ACESSAR OS SISTEMAS UTILIZE AS SEGUINTES INFORMAÇÕES:'
 echo
 echo PARA ACESSAR O ZABBIX INSIRA O SEGUINTE ENDEREÇO EM SEU NAVEGADOR WEB: http://$IP_SERVER/
