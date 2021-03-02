f1330d8d182cd64da0731856c4637c05ec284fd2
edutmiranda <79800172+edutmiranda@users.noreply.github.com>
2021-03-02 11:46:35 -0300

    dss

--- setup.sh
+++ setup.sh
@@ -7,24 +7,16 @@ apt update
 apt install docker-ce -y
 curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-#Reset do ambiente
-rm -r monitoramento2/
-docker rm -f $(docker ps -a -q)
-docker volume prune --force
-git clone https://github.com/mirandait-services/monitoramento2
-#Configuração de senhas bancos de dados
-MYSQL_PASSWORD=$(/lib/cryptsetup/askpass "Insira a senha do serviço de bancos de dados:")
-echo MYSQL_PASSWORD=$MYSQL_PASSWORD > monitoramento2/var.env
 #Identificação do IP principal do servidor de monitoramento
 IP_SERVER=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f7)
-echo IP_SERVER=http://$IP_SERVER:9000/ >> monitoramento2/var.env
+echo IP_SERVER=http://$IP_SERVER:9000/ > monitoramento2/var.env
+#Configuração de senhas bancos de dados
+MYSQL_PASSWORD=$(/lib/cryptsetup/askpass "Insira a senha do serviço de bancos de dados:")
+echo MYSQL_PASSWORD=$MYSQL_PASSWORD >> monitoramento2/var.env
 #Password pepper aleatório para o Graylog
 RANDOMPEPPER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 echo RANDOMPEPPER=$RANDOMPEPPER >> monitoramento2/var.env
-clear
 #Configuração se senha de acesso ao Graylog
-#echo Por gentileza insira a senha de acesso ao Graylog Server:
-#GRAYLOG_PASSWORD=$(stty -echo && head -1 </dev/stdin | tr -d '\n' | sha256sum | cut -d" " -f1)
 GRAYLOG_PASSWORD_TEMP=$(/lib/cryptsetup/askpass "Insira a senha do Graylog:")
 GRAYLOG_PASSWORD=$(echo $GRAYLOG_PASSWORD_TEMP | tr -d '\n' | sha256sum | cut -d" " -f1)
 echo GRAYLOG_PASSWORD=$GRAYLOG_PASSWORD >> monitoramento2/var.env
